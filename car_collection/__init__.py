9fc0cac3d0145908e77e7e967b91073b6ffa3fc3
Aryan Jadhav <aryan.jadhav@gmail.com>
2023-07-11 00:08:36 -0400

    first commit

--- car_collection/__init__.py
+++ car_collection/__init__.py
@@ -1,14 +1,26 @@
 from flask import Flask
+from .api.routes import api
+from .site.routes import site
+from .authentication.routes import auth
 from config import Config
-from.site.routes import site
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from .models import db as root_db
 
 app = Flask(__name__)
 
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
 
+
+app.register_blueprint(api)
 app.register_blueprint(site)
+app.register_blueprint(auth)
 
 app.config.from_object(Config)
+
+root_db.init_app(app)
+
+migrate = Migrate(app, root_db)
+
+
---
+++ car_collection/authentication/routes.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, render_template, request, redirect,url_for,flash
+from car_collection.models import User,db
+from car_collection.forms import UserLoginForm
+
+
+auth = Blueprint('auth', __name__, template_folder = 'auth_templates')
+
+@auth.route('/signup', methods = ['GET', 'POST'])
+def signup():
+    userform = UserLoginForm()
+
+    try:
+        if request.method == 'POST' and userform.validate_on_submit():
+            email = userform.email.data
+            password = userform.password.data
+            print(email,password)
+
+            user = User(email, password)
+
+            db.session.add(user)
+            db.session.commit()
+
+            flash(f'You have successfully created a user account {email}', 'user-created')
+
+            return redirect(url_for('site.home'))
+
+    except:
+        raise Exception('Invalid Form Data: Please Check your form')
+    return render_template('signup.html', form=userform)
+
+@auth.route('/signin', methods = ['GET', 'POST'])
+def signin():
+    return render_template('signin.html')
